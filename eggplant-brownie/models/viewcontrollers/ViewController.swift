fd1fc1774fbae6a31bd80de7137d871f6aafe1e3
thiago zanarelli <thiagozanarelli@MacBook-Pro-de-thiago.local>
2015-09-29 22:51:19 -0300

    final commit

--- eggplant-brownie/models/viewcontrollers/ViewController.swift
+++ eggplant-brownie/models/viewcontrollers/ViewController.swift
@@ -6,141 +6,127 @@
 //  Copyright © 2015 TI IT ZanaLab. All rights reserved.
 //
 
+//
+//  ViewController.swift
+//  eggplant-brownie
+//
+//  Created by Tiago Davi on 14/12/14.
+//  Copyright (c) 2014 Alura. All rights reserved.
+//
+
 import UIKit
 
 protocol AddAMealDelegate {
-    func add(meal: Meal)
+    func add(meal:Meal)
 }
-
 protocol AddAnItemDelegate {
     func addNew(item:Item)
 }
 
-class ViewController: UIViewController, UITableViewDataSource , UITableViewDelegate, AddAnItemDelegate{
-    
-    
-    var items = [ Item(name: "Eggplant Brownie", calories: 10),
-        Item(name: "Zucchini Muffin", calories: 10),
-        Item(name: "Cookie", calories: 10),
-        Item(name: "Coconut oil", calories: 500),
-        Item(name: "Chocolate frosting", calories: 1000),
-        Item(name: "Chocolate chip", calories: 1000)
-    ]
-    
-    
+class ViewController:
+    UIViewController,
+    UITableViewDataSource,
+    UITableViewDelegate,
+AddAnItemDelegate {
     
     @IBOutlet var nameField: UITextField!
     @IBOutlet var happinessField: UITextField!
-    var delegate:AddAMealDelegate?
-    var selected = Array<Item>()
-    
-    
-    
     @IBOutlet var tableView: UITableView?
     
-    func addNew(item: Item) {
-        items.append(item)
-        if let table = tableView {
-        table.reloadData()
-    } else {
-        Alert(controller: self).show()
-        } }
-    
-    func tableView(tableView: UITableView,
-        numberOfRowsInSection section: Int) -> Int {
-        return items.count
-    }
-    
-    func tableView(tableView: UITableView,
-        cellForRowAtIndexPath indexPath: NSIndexPath)
-        -> UITableViewCell {
-        let row = indexPath.row
-        let item = items[ row ]
-        let cell = UITableViewCell(style:
-        UITableViewCellStyle.Default,reuseIdentifier: nil)
-        cell.textLabel!.text = item.name
-        return cell
-    }
-    
-    
-    func find(elements:Array<Item>, toFind:Item) -> Int? {
-        let max = elements.count - 1
-        for i in 0...max {
-        if toFind == elements[i] {
-        return i
-        } }
-        return nil }
-    
-    
-    
-    func tableView(tableView: UITableView,
-            didSelectRowAtIndexPath indexPath: NSIndexPath) {
-            let cell = tableView.cellForRowAtIndexPath(indexPath)
-            if cell == nil {
-            return }
-            if (cell!.accessoryType ==
-            UITableViewCellAccessoryType.None) {
-            cell!.accessoryType =
-            UITableViewCellAccessoryType.Checkmark
-            selected.append(items[indexPath.row])
-        } else {
-            cell!.accessoryType = UITableViewCellAccessoryType.None
-            if let position = find(selected, toFind: items[indexPath.row]) {
-            selected.removeAtIndex(position)
-            }
-            } }
-    
+    var delegate:AddAMealDelegate?
+    var items = Array<Item>()
+    var selected = Array<Item>()
     
     override func viewDidLoad() {
-        let newItemButton = UIBarButtonItem(title: "new item",
-        style: UIBarButtonItemStyle.Plain,
-        target: self,
-        action: Selector("showNewItem"))
+        super.viewDidLoad()
+        
+        let newItemButton = UIBarButtonItem(
+            title: "New Item",
+            style: UIBarButtonItemStyle.Plain,
+            target: self,
+            action: Selector("showNewItem")
+        )
+        
         navigationItem.rightBarButtonItem = newItemButton
+        
+        self.items = Dao().loadItems()
+        
     }
     
     @IBAction func showNewItem(){
         let newItem = NewItemViewController(delegate: self)
         if let navigation = navigationController {
             navigation.pushViewController(newItem, animated: true)
-                   }else{
-                        Alert(controller: self).show()
+        }else{
+            Alert(controller: self).show()
         }
     }
     
     @IBAction func add(){
-            
-            if nameField == nil || happinessField == nil {
-            
+        
+        
+        if nameField == nil || happinessField == nil {
             return
-            
-            }
-            
-            let name = nameField!.text
-            let happiness = Int(happinessField!.text!)
-            
-            if happiness == nil {
-            
+        }
+        
+        let name = nameField!.text
+        let happiness = Int(happinessField!.text!)
+        
+        if happiness == nil{
             return
-            }
-            
-            let meal = Meal(name: name!, happiness: happiness!)
-            meal.items = selected
-            print("eaten: \(meal.name) \(meal.happiness) \(meal.items)!")
-            
-            if delegate == nil {
-                return
-            }
-            
-            delegate!.add(meal)
-            
-            if let navigation = self.navigationController {
-                navigation.popViewControllerAnimated(true)
-            }
-            
-            
-            
+        }
+        if delegate == nil {
+            return
+        }
+        
+        let meal = Meal(name:name!, happiness:happiness!)
+        
+        meal.items = selected
+        
+        delegate!.add(meal)
+        
+        if let navigation = self.navigationController {
+            navigation.popViewControllerAnimated(true)
+        }
+    }
+    func addNew(item: Item) {
+        items.append(item)
+        
+        Dao().saveItems(items)
+        
+        if let table = tableView {
+            table.reloadData()
+        }
+        else {
+            Alert(controller: self).show(message: "Oh My God")
+        }
     }
     
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let row = indexPath.row
+        let item = items[row]
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: nil)
+        if let textLabel = cell.textLabel {
+            textLabel.text = item.name
+        }
+        return cell
+    }
+    func tableView(tableView: UITableView, didDeselectRowAtIndexPath indexPath: NSIndexPath) {
+        let cell = tableView.cellForRowAtIndexPath(indexPath)
+        if cell == nil {
+            return
+        }
+        if cell!.accessoryType == UITableViewCellAccessoryType.None {
+            cell!.accessoryType = UITableViewCellAccessoryType.Checkmark
+            selected.append(items[indexPath.row])
+        }else{
+            cell!.accessoryType = UITableViewCellAccessoryType.None
+            if let position = selected.indexOf(items[indexPath.row]){
+                selected.removeAtIndex(position)
+            }
+        }
+    }
 }
-
